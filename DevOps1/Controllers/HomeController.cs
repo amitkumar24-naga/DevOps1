e72beebd8d5882ab4bc21bdacf9b788b489bb778
amitkumar24 <amit.kumar24@nagarro.com>
2021-08-16 21:59:35 +0530

    Add project files.

---
+++ DevOps1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using DevOps1.Models;
+using DevOps1.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevOps1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPostRepository postRepository;
+
+        public HomeController(IPostRepository _postRepository)
+        {
+            postRepository = _postRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var model = postRepository.GetPosts();
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DevOps1/Repository/IPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using DevOps1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevOps1.Repository
+{
+    public interface IPostRepository
+    {
+        List<PostViewModel> GetPosts();
+    }
+
+}
